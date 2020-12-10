e617eff4e306f534dc6aef6cbf1c2d7c641c82f5
antonio481 <adanilovx@gmail.com>
2020-12-10 17:51:47 +0400

    task01

---
+++ src/ru/danilov/s02/Task04.java
@@ -0,0 +1,7 @@
+package ru.danilov.s02;
+
+public class Task04 {
+    private static double countDistance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+}
