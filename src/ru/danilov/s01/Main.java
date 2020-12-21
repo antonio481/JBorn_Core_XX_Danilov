25a3a2a94930736640523ae4aa515dd7fd499a8c
antonio481 <adanilovx@gmail.com>
2020-12-21 22:19:01 +0400

    task02

--- src/ru/danilov/s01/Main.java
+++ src/ru/danilov/s01/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");// write your code here
+        int[] a = new int[] { 1, 2, 3, 5 };
+        int[] b = new int[] { 1, 1, 1, 1 };
+        b = a;
+        b[0] = 9;
+        System.out.println(b);
+        System.out.println(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
     }
 }
