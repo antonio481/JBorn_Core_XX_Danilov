a362cb325a5dd79b604000668ef62cbf55826184
antonio481 <adanilovx@gmail.com>
2020-12-26 15:11:13 +0400

    task06

--- src/ru/danilov/s03/Task06.java
+++ src/ru/danilov/s03/Task06.java
@@ -1,9 +1,6 @@
 package ru.danilov.s03;
 
 public class Task06 {
-    public static void main(String[] args) {
-        System.out.println(changeHalfOfStrings("symbol"));
-    }
     private static String changeHalfOfStrings(String string) {
         StringBuilder newString=new StringBuilder();
         if (string.length() % 2 == 0) {
