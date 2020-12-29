709d90f689dbe26c24203e39313c772ea01e0faa
antonio481 <adanilovx@gmail.com>
2020-12-29 18:04:08 +0400

    task09

---
+++ src/ru/danilov/s03/Task10.java
@@ -0,0 +1,19 @@
+package ru.danilov.s03;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task10 {
+    public static String gettingPartinLatinLetters(String str){
+        String regex = "[a-zA-z]{2,}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        StringBuilder newString=new StringBuilder();
+
+        while (matcher.find()){
+    newString.append(str, matcher.start(), matcher.end());
+
+    }
+    return newString.toString();
+    }
+}
